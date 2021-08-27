7ca4a63b134341663dae05f9a3f713e24e3a1c9e
Sergio Cortes <sergiocortessat@gmail.com>
2021-08-27 13:54:21 +0200

    Add latest changes to post measuremnt

--- src/components/AddMeasurement.js
+++ src/components/AddMeasurement.js
@@ -1,15 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useAuth0 } from '@auth0/auth0-react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUsers, postMeasurements } from '../API/API';
+import { updateUser } from '../Redux/Actions';
 
 const AddMeasurement = () => {
   const { coins } = useSelector((state) => state.coins);
-  const [coin, setCoin] = useState('Submit');
+  const [coin, setCoin] = useState(1);
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.user);
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    getUsers().then((res) => {
+      const filtered = res.filter((a) => a.sub === user.sub);
+      dispatch(updateUser(filtered[0]));
+    });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target.id);
+    getAccessTokenSilently()
+      .then((accessToken) => {
+        postMeasurements({ user_id: currentUser.id, goal_id: coin, unit: 1 }, accessToken);
+      });
   };
+
   return (
   // create a form with list items and
     <form action="/action_page.php">
@@ -20,7 +37,7 @@ const AddMeasurement = () => {
             <option
               key={coin.id}
               value={coin.name}
-              onClick={() => setCoin(coin.name)}
+              onClick={() => setCoin(coin.id)}
             >
               {coin.name}
             </option>
