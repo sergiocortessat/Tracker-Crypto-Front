27f35a3de5a59fe67ff9f585c2a1da6680817d51
Sergio Cortes <sergiocortessat@gmail.com>
2021-08-30 19:24:27 +0200

    Add percentage to show measurements

--- src/components/Measurement.js
+++ src/components/Measurement.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable array-callback-return */
 /* eslint-disable no-unused-vars */
 import { useAuth0 } from '@auth0/auth0-react';
 import React, { useEffect, useState } from 'react';
@@ -13,12 +15,23 @@ const Measurement = () => {
   const [measures, setMeasures] = useState();
   const [temp, setTemp] = useState(false);
   const [goal, setGoal] = useState();
+  const [percentage, setPercentage] = useState(0);
   useEffect(() => {
     getGoal(user.sub).then((res) => {
       const temp = res.filter((m) => m.coin_id === Number(id))[0];
       setGoal(temp.goal);
       setMeasures(temp.measurements);
     });
+    let result = 0;
+    measures ? measures.map((measure) => {
+      if (measure.length > 0) {
+        measure.map((measurement) => {
+          measurement.unit ? (result += measurement.unit) : (result);
+        });
+      }
+    }) : 0;
+    const percentage = (goal / result) * 100;
+    setPercentage(percentage);
   }, []);
 
   const handleDelete = (id) => {
@@ -29,7 +42,20 @@ const Measurement = () => {
       window.location.reload();
     }, 1000);
   };
-  console.log(goal);
+
+  const [aSum, setASum] = useState(0);
+  const sum = (measures) => {
+    let result = 0;
+    measures.map((measure) => {
+      if (measure.length > 0) {
+        measure.map((measurement) => {
+          measurement.unit ? (result += measurement.unit) : (result);
+        });
+      }
+    });
+    const percentage = (goal / result) * 100;
+    setPercentage(percentage);
+  };
   return (
     <>
       <div className="main-progress">
